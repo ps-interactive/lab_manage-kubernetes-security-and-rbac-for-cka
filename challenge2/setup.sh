935cc6aa809f8d6f2ed9d7762161bcacee36c7b8
Antonio Piedra <35215298+ajpiedra@users.noreply.github.com>
2024-11-10 17:37:40 +0100

    Create setup.sh

---
+++ challenge2/setup.sh
@@ -0,0 +1,32 @@
+# Create users and set password
+sudo useradd -m user1 && echo 'user1:12345' | sudo chpasswd
+sudo useradd -m user2 && echo 'user2:12345' | sudo chpasswd
+
+# Create .kube folders
+mkdir -p /home/user1/.kube
+mkdir -p /home/user1/.kube
+
+# Generate private key for users
+openssl genrsa -out /home/user1/.kube/user1.key 2048
+openssl genrsa -out /home/user2/.kube/user2.key 2048
+
+# Create CSR for users
+openssl req -new -key /home/user1/.kube/user1.key -out /home/user1/.kube/user1.csr -subj "/CN=user1/O=group1"
+openssl req -new -key /home/user2/.kube/user2.key -out /home/user2/.kube/user2.csr -subj "/CN=user2/O=group2"
+
+# Sign the CSR with Minikube's CA
+openssl x509 -req -in /home/user1/.kube/user1.csr -CA /home/pslearner/.minikube/ca.crt -CAkey /home/pslearner/.minikube/ca.key -CAcreateserial -out /home/user1/.kube/user1.crt -days 365
+openssl x509 -req -in /home/user2/.kube/user2.csr -CA /home/pslearner/.minikube/ca.crt -CAkey /home/pslearner/.minikube/ca.key -CAcreateserial -out /home/user2/.kube/user2.crt -days 365
+
+# Copy minikube CA to user dirs
+cp /home/pslearner/.minikube/ca.crt /home/user1/.kube/ca.crt
+cp /home/pslearner/.minikube/ca.crt /home/user2/.kube/ca.crt
+
+# Move kubeconfig files to users dirs
+mv /home/pslearner/resources/challenge2/kubeconfig-user1.yaml /home/user1/.kube/config
+mv /home/pslearner/resources/challenge2/kubeconfig-user2.yaml /home/user2/.kube/config
+
+# Provide perms to users
+sudo chown user1:user1 /home/user1
+sudo chown user2:user2 /home/user2
+
